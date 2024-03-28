d858f046c9760103f13082e7cd0520f2bb118e50
jakzoe <155812065+jakzoe@users.noreply.github.com>
2024-03-28 12:32:06 +0100

    feat!: save wavelengths once per dataset, not per measurement

--- slay/laser_constants.py
+++ slay/laser_constants.py
@@ -1,20 +1,20 @@
 ## === Einstellung des Spektroskops ===
 
-INTTIME = 1   # 1-498000 ms
-SCAN_AVG = 1   # > 1
-SMOOTH = 0     # 1-4
-XTIMING = 3    # 1-4
+INTTIME = 1  # 1-498000 ms
+SCAN_AVG = 1  # > 1
+SMOOTH = 0  # 1-4
+XTIMING = 3  # 1-4
 
 ## === Einstellung des Diagramms ===
 
-NAME = "TETETSRT Langzeitmessung mit Superkontinuumlaser mit Blaulichtlasern"
-# neue Messungen überschreiben alte
+NAME = "ZZZ Langzeitmessung mit Superkontinuumlaser mit Blaulichtlasern"
+# neue Messungen überschreiben alte Messungen
 OVERWRITE = True
 
 ## === Einstellungen des Lasers ===
 
 # wie häufig eine Messung wiederholt wird
-REPETITIONS = 10 #  29.8 TiB for an a
+REPETITIONS = 10  #  29.8 TiB for an a
 
 ## ACHTUNG: DELAY und IRRADITION_TIME sollten MINDESTENS 3 ms, sein, der Arduino schafft es nicht in kürzerer Zeit, alles anzustellen
 # Zeit in ms, die zwischen jeder Messung gewartet werden soll
@@ -26,7 +26,7 @@ ARDUINO_DELAY = 3
 # wie stark der Laser eingestellt ist (z. B. Frequenz oder Spannung, je nach Lasertyp)
 INTENSITY = 100
 # Laser durchgängig angeschaltet lassen oder nicht
-CONTINOUS = False
+CONTINOUS = True
 # ob ein Graufilter dazwischen ist oder nicht
 GRAUFILTER = False
 # Sekunden, nach denen die Messung, unabhängig von REPETITIONS, beendet werden soll
@@ -38,7 +38,7 @@ if GRAUFILTER:
 
 NAME += " mit " + str(INTENSITY) + " %"
 
-
+DEBUG = False
 
 # === Indices ===
 INTTIME_INDEX = 0
@@ -47,6 +47,6 @@ SCAN_AVG_INDEX = 2
 SMOOTH_INDEX = 3
 XTIMING_INDEX = 4
 REPETITIONS_INDEX = 5
-DELAY_INDEX = 6
+ARDUINO_DELAY_INDEX = 6
 IRRADITION_TIME_INDEX = 7
 CONTINOUS_INDEX = 8
--- slay/laser_messungen.py
+++ slay/laser_messungen.py
@@ -21,14 +21,12 @@ except:
     print("\n Failed to load the stellarnet library.\n")
     # exit()
 
-DEBUG = False
-
 spectrometer = None
 wav = None
 arduino = None
 
 if DEBUG:
-    wav = np.zeros((2048, 1))
+    wav = np.zeros((2048,))
     for i in range(len(wav)):
         wav[i] = 300 + i / 5.0
 
@@ -180,13 +178,14 @@ def spectrometer_setup():
     global spectrometer, wav
 
     spectrometer, wav = sn.array_get_spec(0)
+    wav = wav.reshape(
+        2048,
+    )
 
     print(spectrometer)
-    sn.ext_trig(spectrometer, True)
     print("\nDevice ID: ", sn.getDeviceId(spectrometer))
 
     sn.ext_trig(spectrometer, True)
-
     # True ignoriert die ersten Messdaten, da diese ungenau sein können (durch Änderung der Integrationszeit)
     sn.setParam(spectrometer, INTTIME, SCAN_AVG, SMOOTH, XTIMING, True)
 
@@ -196,8 +195,11 @@ def get_data():
     # var = sn.array_spectrum(spectrometer, wav)
     # print((time.time() - start_time) * 1000)
     # return var
-    return sn.array_spectrum(spectrometer, wav)
-    # return sn.getSpectrum_Y(spectrometer)
+    # print(sn.array_spectrum(spectrometer, wav).shape)  # (2048, 2)
+    # print(sn.getSpectrum_Y(spectrometer).shape)  # (2048,)
+
+    # return sn.array_spectrum(spectrometer, wav)
+    return sn.getSpectrum_Y(spectrometer)
 
 
 def plot_results(
@@ -232,36 +234,41 @@ def plot_results(
 
         colors = plt.cm.jet(np.linspace(0, 1, len(file_list) + 2))
 
-        temp_data = np.load(file_list[0])["arr_0"][0]
-        # print(temp_data.shape)
-        # exit()
-        temp_waves = []
-        for val in temp_data:
-            temp_waves.append(val[0])
+        # temp_data = np.load(file_list[0])["arr_0"][0]
+        # # print(temp_data.shape)
+        # # exit()
+        # temp_waves = []
+        # for val in temp_data:
+        #     temp_waves.append(val[0])
 
-        temp_waves = np.array(temp_waves)
+        # temp_waves = np.array(temp_waves)
+
+        wavelengths = np.load(file_list[0])["arr_1"]
+        print("waveshape")
+        print(wavelengths.shape)
 
         if setting.zoom_end == sys.maxsize:
             setting.zoom_end = 2048
         else:
-            setting.zoom_end = (np.abs(temp_waves - setting.zoom_end)).argmin()
+            setting.zoom_end = (np.abs(wavelengths - setting.zoom_end)).argmin()
 
         if setting.zoom_start != 0:
-            setting.zoom_start = (np.abs(temp_waves - setting.zoom_start)).argmin()
+            setting.zoom_start = (np.abs(wavelengths - setting.zoom_start)).argmin()
 
         x_ax_len = setting.zoom_end - setting.zoom_start
 
-        result = np.zeros([x_ax_len, 2], dtype=float)
+        result = np.zeros([x_ax_len], dtype=float)
         standard_deviation_data = []
         if verbose:
             print("calculating average...")
+
         for file in file_list:
 
             if verbose:
                 print("reading data from disk...")
             loaded_array = np.load(file)
             spectrometer_data = loaded_array["arr_0"]
-            metadata = loaded_array["arr_1"]
+            metadata = loaded_array["arr_2"]
 
             assert metadata[REPETITIONS_INDEX] == len(spectrometer_data)
 
@@ -273,7 +280,7 @@ def plot_results(
             if setting.normalize_power:
                 normalize_integrationtime_factor = metadata[INTENSITY_INDEX]
 
-            mean = np.zeros([x_ax_len, 2], dtype=float)
+            mean = np.zeros([x_ax_len], dtype=float)
 
             for i in range(len(spectrometer_data)):
                 j = i + setting.intervall_start
@@ -285,7 +292,7 @@ def plot_results(
                     if l >= setting.zoom_end:
                         break
                     intensity.append(
-                        spectrometer_data[j][l][1]
+                        spectrometer_data[j][l]
                         / normalize_integrationtime_factor
                         / normalize_power
                     )
@@ -309,24 +316,22 @@ def plot_results(
         result /= len(file_list)
         standard_deviation = np.std(standard_deviation_data, axis=0)
 
-        wavelength = []
-        intensity = []
-        for val in result:
-            wavelength.append(val[0])
-            intensity.append(val[1])
+        wave_min = min(wavelengths)
+        wave_max = max(wavelengths)
 
-        wave_min = min(wavelength)
-        wave_max = max(wavelength)
+        assert wave_min == wavelengths[0]
+        assert wave_max == wavelengths[-1]
+        print("true! change accordingly for effiency")
 
-        intensity_min = min(intensity)
-        intensity_max = max(intensity)
+        intensity_min = min(result)
+        intensity_max = max(result)
         std_max = max(standard_deviation)
 
         if verbose:
             print("creating backgroundimage...")
         # wird resized, deshalb height=1
         ax.imshow(
-            make_spectrum_image(int(len(wavelength)), 1, wavelength),
+            make_spectrum_image(int(len(wavelengths)), 1, wavelengths),
             extent=[
                 wave_min,
                 wave_max,
@@ -345,19 +350,23 @@ def plot_results(
             rate = setting.intervall_end - setting.intervall_start
 
         ax.scatter(
-            wavelength,
-            intensity,
+            wavelengths,
+            result,
             label="Mittelwert von {0} Messungen".format(rate),
             color=colors[iterator],
             s=1,
         )
-        print(len(intensity), len(standard_deviation))
+        print(len(result), len(standard_deviation))
 
-        assert len(intensity) == len(standard_deviation)
+        assert len(result) == len(standard_deviation)
+
+        print(wavelengths.shape)
+        print(result.shape)
+
         ax.fill_between(
-            wavelength,
-            intensity - standard_deviation,
-            intensity + standard_deviation,
+            wavelengths,
+            result - standard_deviation,
+            result + standard_deviation,
             alpha=0.5,
             edgecolor="#CC4F1B",
             facecolor="#FF9848",
@@ -560,9 +569,16 @@ if __name__ == "__main__":
 
     if DEBUG:
         NAME += "_DEBUG"
-        DIR_PATH = "Messungen/Debug/" + NAME + "/"
+        DIR_PATH = (
+            os.path.dirname(os.path.realpath(__file__))
+            + "/Messungen/Debug/"
+            + NAME
+            + "/"
+        )
     else:
-        DIR_PATH = "Messungen/" + NAME + "/"
+        DIR_PATH = (
+            os.path.dirname(os.path.realpath(__file__)) + "/Messungen/" + NAME + "/"
+        )
 
     try:
         os.makedirs(DIR_PATH, 0o777)
@@ -575,6 +591,7 @@ if __name__ == "__main__":
         ## bitte entfernen.
         if not CONTINOUS:
             arduino_setup("/dev/ttyUSB0", 1)
+            turn_on_laser()
 
     """
     measurements = []
@@ -584,16 +601,13 @@ if __name__ == "__main__":
         time.sleep(DELAY / 1000.0)
     """
     # minimal schneller als jedes Mal list.append()
-    measurements = np.zeros([REPETITIONS, 2048, 2], dtype=float)
-
-    if CONTINOUS and not DEBUG:
-        turn_on_laser()
+    measurements = np.zeros([REPETITIONS, 2048], dtype=float)
 
     seconds = time.time()
 
     if DEBUG:
         for i in range(REPETITIONS):
-            measurements[i] = np.zeros([2048, 2], dtype=float)
+            measurements[i] = np.zeros([2048], dtype=float)
     elif CONTINOUS:
         for i in range(REPETITIONS):
             measurements[i] = get_data()
@@ -638,17 +652,20 @@ if __name__ == "__main__":
     metadata[SMOOTH_INDEX] = SMOOTH
     metadata[XTIMING_INDEX] = XTIMING
     metadata[REPETITIONS_INDEX] = REPETITIONS
-    metadata[DELAY_INDEX] = DELAY
+    metadata[ARDUINO_DELAY_INDEX] = ARDUINO_DELAY
     metadata[IRRADITION_TIME_INDEX] = IRRADITION_TIME
     metadata[CONTINOUS_INDEX] = int(CONTINOUS)
 
     if OVERWRITE:
+        file_name = DIR_PATH + "Messung"
+    else:
         # manche Dateisysteme unterstützen keinen Doppelpunkt im Dateinamen
         file_name = DIR_PATH + str(datetime.now()).replace(":", "_")
-    else:
-        file_name = DIR_PATH + "Messung"
 
-    np.savez_compressed(file_name, np.array(measurements), np.array(metadata))
+    print(DIR_PATH)
+    np.savez_compressed(
+        file_name, np.array(measurements), np.array(wav), np.array(metadata)
+    )
     os.chmod(file_name + ".npz", 0o777)
 
     plot_results([PlottingSettings(DIR_PATH)])
