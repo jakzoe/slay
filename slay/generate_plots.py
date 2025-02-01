94e1b714464330be3a99f5e3ad3ba50d3e03c799
jakzoe <155812065+jakzoe@users.noreply.github.com>
2025-02-01 21:39:01 +0100

    feat(plot gen): support measurement filtering via whitelist/blacklist

--- slay/generate_plots.py
+++ slay/generate_plots.py
@@ -7,10 +7,21 @@ import shutil
 if __name__ == "__main__":
 
     delte_old_pictures = True
+    # um schnell bestimmtes zu exkludieren
+    plot_general = True
+    plot_fluo = True
+    plot_time_slices = True
+
+    # nur bestimmtes plotten. Leer ist disable (alles plotten). Enthält Keyword, welches in dem Namen sein muss.
+    plot_list = ["Bodensatz"]
+    blacklist = False  # black- oder whitelist
 
     # die ganzen Symblinks löschen
     if delte_old_pictures:
-        shutil.rmtree("plots/")
+        try:
+            shutil.rmtree("plots/")
+        except FileNotFoundError:
+            print("plots dir was already deleted")
 
     plot = Laserplot()
     paths = []
@@ -20,6 +31,15 @@ if __name__ == "__main__":
             paths.append(os.path.join(root, dir, subdir))
 
     for path in paths:
+        # ob das Element in der white/blacklist ist
+        if plot_list and (
+            (blacklist and any(ele in path for ele in plot_list))
+            or (not blacklist and any(ele not in path for ele in plot_list))
+        ):
+            continue
+
+        print(path)
+
         names = [
             os.path.splitext(f)[0]
             for f in os.listdir(path)
@@ -34,7 +54,10 @@ if __name__ == "__main__":
                 os.remove(os.path.join(path, pic_name + ".png"))
 
         for name in names:
-            print(path, name)
+
+            # grüner Text (\033[ ist Escape sequence start, 32m Green color code, 0m color reset)
+            print("\033[32m" + name + "\033[0m")
+
             p_settings = []
 
             m_settings = MeasurementSettings.from_json(
@@ -42,122 +65,80 @@ if __name__ == "__main__":
             )
 
             # Generellen Durchschnitt plotten
-            p_settings.append([PlottingSettings(path, name, True, scatter=True)])
+            if plot_general:
+                p_settings.append([PlottingSettings(path, name, True, scatter=True)])
 
             # Fluoreszenz-Peak plotten (zwischen 720 und 740 nm)
-            p_settings.extend(
-                (
-                    [
-                        PlottingSettings(
-                            path, name, True, zoom_start=730, zoom_end=731, scatter=True
-                        )
-                    ],
-                    [
-                        PlottingSettings(
-                            path, name, True, zoom_start=750, zoom_end=751, scatter=True
-                        )
-                    ],
-                    [
-                        PlottingSettings(
-                            path, name, True, zoom_start=740, zoom_end=741, scatter=True
-                        )
-                    ],
+            if plot_fluo:
+                p_settings.extend(
+                    (
+                        [
+                            PlottingSettings(
+                                path,
+                                name,
+                                True,
+                                zoom_start=730,
+                                zoom_end=731,
+                                scatter=True,
+                            )
+                        ],
+                        [
+                            PlottingSettings(
+                                path,
+                                name,
+                                True,
+                                zoom_start=750,
+                                zoom_end=751,
+                                scatter=True,
+                            )
+                        ],
+                        [
+                            PlottingSettings(
+                                path,
+                                name,
+                                True,
+                                zoom_start=740,
+                                zoom_end=741,
+                                scatter=True,
+                            )
+                        ],
+                    )
                 )
-            )
 
-            # einzelne Zeitabschnitte plotten
-            p_settings.append(
-                [
-                    PlottingSettings(
-                        path,
-                        name,
-                        True,
-                        interval_start=0,
-                        interval_end=1 / 3,
-                        scatter=False,
-                        line_style="-",
-                        color="black",
-                    ),
-                    PlottingSettings(
-                        path,
-                        name,
-                        True,
-                        interval_start=1 / 3,
-                        interval_end=2 / 3,
-                        line_style="--",
-                        color="red",
-                    ),
-                    PlottingSettings(
-                        path,
-                        name,
-                        True,
-                        interval_start=2 / 3,
-                        interval_end=3 / 3,
-                        line_style=":",
-                        color="blue",
-                    ),
-                ]
-            )
+            # # einzelne Zeitabschnitte plotten
+            if plot_time_slices:
+                p_settings.append(
+                    [
+                        PlottingSettings(
+                            path,
+                            name,
+                            True,
+                            interval_start=0,
+                            interval_end=1 / 3,
+                            scatter=False,
+                            line_style="-",
+                            color="black",
+                        ),
+                        PlottingSettings(
+                            path,
+                            name,
+                            True,
+                            interval_start=1 / 3,
+                            interval_end=2 / 3,
+                            line_style="--",
+                            color="red",
+                        ),
+                        PlottingSettings(
+                            path,
+                            name,
+                            True,
+                            interval_start=2 / 3,
+                            interval_end=3 / 3,
+                            line_style=":",
+                            color="blue",
+                        ),
+                    ]
+                )
 
             for p in p_settings:
                 plot.plot_results(p, m_settings, show_plots=False)
-
-    exit()
-
-    # path = "messungen/Neutral_445/Kontinuierlich/"
-    # name = "2025-01-26 18_59_02.972938"
-    # path = "messungen/Chlorophyll_Ohne_Amp_Rhombus_Unfokussiert_Blub/Kontinuierlich/"
-    # name = "2025-01-26 13_25_44.753531"
-    # path = "messungen/Chlorophyll_Ohne_Amp_Rhombus_Unfokussiert_Mit_Absatz_Aber_Also_Doof/Kontinuierlich/"
-    # name = "2025-01-26 20_39_36.791213"
-    # path = "messungen/Chlorophyll_Ohne_Amp_Rhombus_Unfokussiert_Nur_Bodensatz_Also_Toll/Kontinuierlich/"
-    # name = "2025-01-26 22_49_50.736128"
-    # path = "messungen/Chlorophyll_Ohne_Amp_Rhombus_Unfokussiert/Kontinuierlich/"
-    # name = "2025-01-27 10_46_45.346951"
-    path = "messungen/Radieschen_Direkt_Unfokussiert/Kontinuierlich/"
-    name = "2025-01-26 17_37_51.172408"
-
-    mSettings = MeasurementSettings.from_json(path + name + ".json")
-    # plot.plot_results(
-    #     [PlottingSettings(path, name, True, zoom_start=750, zoom_end=751, scatter=True)]
-    # )
-
-    plot.plot_results(
-        [
-            PlottingSettings(
-                path,
-                name,
-                True,
-                # zoom_start=750,
-                # zoom_end=751,
-                interval_start=0,
-                interval_end=1200,
-                scatter=False,
-                line_style="-",
-                color="black",
-            ),
-            PlottingSettings(
-                path,
-                name,
-                True,
-                # zoom_start=750,
-                # zoom_end=751,
-                interval_start=1200,
-                interval_end=2400,
-                line_style="--",
-                color="red",
-            ),
-            PlottingSettings(
-                path,
-                name,
-                True,
-                # zoom_start=750,
-                # zoom_end=751,
-                interval_start=2_400,
-                interval_end=3_600,
-                line_style=":",
-                color="blue",
-            ),
-        ],
-        mSettings,
-    )
