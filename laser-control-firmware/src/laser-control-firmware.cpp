cd2508e8b082539d7c24dc7c1026dbc8b1997a22
jakzoe <155812065+jakzoe@users.noreply.github.com>
2025-06-16 16:02:38 +0200

    feat(firmware): move from Arduino to ESP32

--- laser-control-firmware/src/laser-control-firmware.cpp
+++ laser-control-firmware/src/laser-control-firmware.cpp
@@ -1,147 +1,146 @@
-
 #include <Arduino.h>
 
-// https://roboticsbackend.com/arduino-fast-digitalwrite/ als Alternative
-// #include <digitalWriteFast.h>
+// UART1 nutzen (gibt kein UART2 bei C3)
+HardwareSerial LaserSerial(1);
+
+// ESP32-C3 haben 6 channels (https://docs.espressif.com/projects/arduino-esp32/en/latest/api/ledc.html)
+// Pins, die einen Channel teilen, teilen automatisch das generierte PWM-Signal
+// C3 hat nur slow speed mode
+#define PWM_CHANNEL_405 0
+uint16_t pwmFreq405 = 2000; // 80_000_000 / 2**14 = 4882.8125, aber ab 4.5 kHz geht nichts mehr. Vermutlich macht da das MOSFET nicht mehr mit.
+// geht theoretisch bis 14, dann funktioniert es aber ab ca. 75 Prozent Duty nicht mehr.
+// (zumindest bei dem 405 nm Laser sind zwar bei dem TTL-Eingang auch 14 bit möglich,
+// bei einer direkten Steuerung des Stroms mittels eines MOSFETs aber nicht. Ggf. liegt das auch an dem MOSFET IRF510N)
+uint16_t pwmResBits405 = 13;
+uint16_t maxDutyVal405 = (uint16_t)(pow(2, pwmResBits405) - 1);
+uint16_t pwmDutyVal405 = 0;
+
+#define PWM_CHANNEL_445 4
+uint16_t pwmFreq445 = 2000;
+uint16_t pwmResBits445 = 13;
+uint16_t maxDutyVal445 = (uint16_t)(pow(2, pwmResBits445) - 1);
+uint16_t pwmDutyVal445 = 0;
 
 // PWM Pin
-#define LASER_PIN_405 5
-#define LASER_PIN_SUPERCON 4
-#define LASER_PIN_445_KILL_SWITCH 8
-#define LED_R_PIN 9
-#define LED_G_PIN 10
-#define LED_B_PIN 12                // ich weiß nicht, wieso. Wahrscheinlich wegen der Register. Aber Pin 11 steuert die Brightness des 445 nm Lasers...
+#define LASER_PIN_405 8
+#define LASER_PIN_445 2
+// wenn der 445 nm Laser kein PWM erhält, erreicht er die maximale Leistung.
+#define DISABLE_PWM_445 false
+#define LASER_PIN_SUPERCON 10
+#define LASER_PIN_445_KILL_SWITCH 9
+#define RElAY_405 3
+#define RElAY_445 1
+// nicht genug pins. Daher entweder Relays oder LED benutzen
+#define USE_RGB_LED false
+#define LED_R_PIN 1
+#define LED_G_PIN 3
+#define LED_B_PIN 1                 // blue disablen (zu wenig Pins)            // ich weiß nicht, wieso. Wahrscheinlich wegen der Register. Aber Pin 11 steuert die Brightness des 445 nm Lasers...
 #define SERIAL_DATA_LENGTH 18       // ein Arduino long hat maximal 10 Ziffern. Plus die 6 für den Namen, ein für das = (6+1 Prefix also) und einen für den Null Characer
 #define SERIAL_DATA_PREFIX_LENGTH 7 // SERIAL_DATA_LENGTH - SERIAL_DATA_PREFIX_LENGTH ist demnach die maximal mögliche Anzahl an Ziffern
+#define POT_PIN 0
+#define POTI_SAMPLE_COUNT 51
+int potiReadings[POTI_SAMPLE_COUNT];
 
-void osp_setup(uint8_t cycles);
-void osp_setup();
+// 0: Laser aus
+// 1: Laser an
+// 2: Es wird auf Daten zum setzen von Parametern gewartet.
+// 3: watchdog updaten (like a dead man's switch press)
+char mode = 'z'; // random init value
+bool modeChanged = false;
+
+// default
+bool continuousMeasurement = true;
+// Timeout zum Abschalten der Laser
+// (wenn das Timeout verstrichen ist, wird angenommen, dass die Verbindung zur Steuersoftware unterbrochen wurde und kein laserOff Signal mehr empfangen werden kann)
+unsigned long expectedDelay = 0;
+unsigned long lastUpdateTime = 0;
+
 void readSerial();
 void setLED(byte r, byte g, byte b);
 void enableLocks();
 void disableLocks();
 void turnLasersOn();
 void turnLasersOff();
-
-// PWM-Signal zwischen 0-255
-uint8_t laser405Brightness = 0;
-unsigned int laser445PulseNum = 0;
-unsigned int laser445PulseDelay = -1;
-
-#define LASER_PIN_445 3
-// für 445 LASER nur manuelles PWM Signal setzen
-#define MODE_SWICH_445 0
-
-// https://github.com/bigjosh/TimerShot/blob/master/TimerShot.ino
-
-#define OSP_SET_WIDTH(cycles) (OCR2B = 0xff - (cycles - 1))
-
-// Setup the one-shot pulse generator and initialize with a pulse width that is (cycles) clock counts long
-
-void osp_setup(uint8_t cycles)
-{
-
-  TCCR2B = 0; // Halt counter by setting clock select bits to 0 (No clock source).
-              // This keeps anyhting from happeneing while we get set up
-
-  TCNT2 = 0x00;          // Start counting at bottom.
-  OCR2A = 0;             // Set TOP to 0. This effectively keeps us from counting becuase the counter just keeps reseting back to 0.
-                         // We break out of this by manually setting the TCNT higher than 0, in which case it will count all the way up to MAX and then overflow back to 0 and get locked up again.
-  OSP_SET_WIDTH(cycles); // This also makes new OCR values get loaded frm the buffer on every clock cycle.
-
-  TCCR2A = _BV(COM2B0) | _BV(COM2B1) | _BV(WGM20) | _BV(WGM21); // OC2B=Set on Match, clear on BOTTOM. Mode 7 Fast PWM.
-  TCCR2B = _BV(WGM22) | _BV(CS20);                              // Start counting now. WGM22=1 to select Fast PWM mode 7
-
-  DDRD |= _BV(3); // Set pin to output (Note that OC2B = GPIO port PD3 = Arduino Digital Pin 3)
-}
-
-// Setup the one-shot pulse generator
-
-void osp_setup()
-{
-
-  osp_setup(laser445PulseNum);
-}
-
-// Fire a one-shot pulse. Use the most recently set width.
-
-#define OSP_FIRE() (TCNT2 = OCR2B - 1)
-
-// Test there is currently a pulse still in progress
-
-#define OSP_INPROGRESS() (TCNT2 > 0)
-
-// Fire a one-shot pusle with the specififed width.
-// Order of operations in calculating m must avoid overflow of the unint8_t.
-// TCNT2 starts one count lower than the match value becuase the chip will block any compare on the cycle after setting a TCNT.
-
-#define OSP_SET_AND_FIRE(cycles)     \
-  {                                  \
-    uint8_t m = 0xff - (cycles - 1); \
-    OCR2B = m;                       \
-    TCNT2 = m - 1;                   \
-  }
-
-// 0: Laser aus
-// 1: Laser an
-// 2: Es wird auf Daten zum setzen von Parametern gewartet.
-char mode = 'z';
-bool modeChanged = false;
-
-// default
-bool continuousMeasurement = true;
-// Timeout zum Abschalten der Laser (wenn das Timeout verstrichen ist, wird angenommen, dass die Verbindung zur Steuersoftware unterbrochen wurde und kein laserOff Signal mehr empfangen werden kann)
-unsigned long expectedDelay = 0;
-unsigned long lastUpdateTime = 0;
+int readPoti();
 
 void setup()
 {
 
-  pinMode(LED_R_PIN, OUTPUT);
-  pinMode(LED_G_PIN, OUTPUT);
-  pinMode(LED_B_PIN, OUTPUT);
+  // input voltage rangem, ADC_11db ist 0-3.3 V (halt ESP32 Pin)
+  analogSetAttenuation(ADC_11db);
+  pinMode(POT_PIN, INPUT);
+
+  if (USE_RGB_LED)
+  {
+    pinMode(LED_R_PIN, OUTPUT);
+    pinMode(LED_G_PIN, OUTPUT);
+    pinMode(LED_B_PIN, OUTPUT);
+  }
+  else
+  {
+    pinMode(RElAY_405, OUTPUT);
+    pinMode(RElAY_445, OUTPUT);
+    // die Relays sind active low
+    digitalWrite(RElAY_405, LOW);
+    digitalWrite(RElAY_445, LOW);
+  }
 
   pinMode(LASER_PIN_SUPERCON, OUTPUT);
   pinMode(LASER_PIN_445_KILL_SWITCH, OUTPUT);
   pinMode(LASER_PIN_405, OUTPUT);
-
   pinMode(LASER_PIN_445, OUTPUT);
 
+  if (!DISABLE_PWM_445)
+  {
+    ledcAttachChannel(LASER_PIN_445, pwmFreq445, pwmResBits445, PWM_CHANNEL_445);
+  }
+
+  ledcAttachChannel(LASER_PIN_405, pwmFreq405, pwmResBits405, PWM_CHANNEL_405);
+
+  // ledcAttachChannel attached die Pins auch, daher direkt ausschalten
   enableLocks();
-  // die default 115200 überschreiben, die von der Library gesetzt werden.
-  // ansonsten gehen bei Processing Werte verloren.
-  Serial.begin(9600); // 115200
-  Serial.println("Alive!");
 
-  // // Phase-Correct PWM with duty cicle 1 over 255 https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm/
-  // pinMode(3, OUTPUT);
-  // pinMode(11, OUTPUT);
-  // TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
-  // TCCR2B = _BV(CS22);
-  // OCR2A = 1;
-  // OCR2B = 1;
+  // Serial.begin(115200);
 
-  if (!MODE_SWICH_445)
-    osp_setup();
+  // UART0 ist beim Debugging nicht verfügbar. Daher UART1 für simultanes Serial benutzen.
+  // Dazu die default pins 20 und 21 von UART0 detachen (automatisch) und an UART1 attachen.
+  // (andere Pins gingen auch, aber 20 und 21 können sowieso weder PWM, noch sind sie analoge Pins etc.)
+
+  // Serial.end(); // müsste es selbst tun
+  // use default 8 bits 1 stop bit, no parity
+  LaserSerial.begin(115200, SERIAL_8N1, 20, 21);
+  LaserSerial.flush();
+  LaserSerial.println("TEST");
+  LaserSerial.begin(115200, SERIAL_8N1, 20, 21);
+
+  // // TEST
+  // turnLasersOn();
 }
 
 void loop()
 {
+  // int potiValue = readPoti();
+  // int mapping = map(readPoti(), 0, 4095, 0, maxDutyVal405);
+  // ledcWrite(LASER_PIN_445, mapping);
+  // ledcWrite(LASER_PIN_405, maxDutyVal405);
+  // // LaserSerial.println("Poti Value:");
+  // // LaserSerial.println(potiValue);
+  // // delay(10);
+  // // LaserSerial.println(mapping);
+  // // delay(10);
+  // // LaserSerial.println(maxDutyVal405);
+  // delay(20);
+  // return;
+
   // disableLocks();
-  // laser445PulseNum = 50;
-  // osp_setup();
-
-  // while (true) {
-  //   //OSP_SET_AND_FIRE(5);
-  //   OSP_FIRE();
-
-  //   while (OSP_INPROGRESS())
-  //     ;  // This just shows how you would wait if nessisary - not nessisary in this application.
-
-  //   // analogWrite(11, 40);  // 30
-  //   delay(20);
-  // }
   readSerial();
+
+  // watchdog
+  if (millis() - lastUpdateTime >= expectedDelay)
+  {
+    turnLasersOff();
+    mode = '0';
+  }
 }
 
 char varSerialData[SERIAL_DATA_LENGTH];
@@ -163,20 +162,20 @@ void readSerial()
 
   if (!modeChanged)
   {
-    if (Serial.available() == 0)
+    if (LaserSerial.available() == 0)
       return;
 
     // // ist fünf schon zu viel?? Länge mal ausgeben lassen
-    // byte m = Serial.readBytesUntil('\n', varSerialData, 15);
+    // byte m = LaserSerial.readBytesUntil('\n', varSerialData, 15);
     // // clear buffer
-    // // while (Serial.available() > 0)
-    // //   Serial.read();
+    // // while (LaserSerial.available() > 0)
+    // //   LaserSerial.read();
 
     // varSerialData[m] = '\0';
-    // // Serial.println(varSerialData);
+    // // LaserSerial.println(varSerialData);
 
-    char newMode = Serial.read();
-    // Serial.println(newMode);
+    char newMode = LaserSerial.read();
+    // LaserSerial.println(newMode);
 
     // update (bei einem continuousMeasurement)
     if (continuousMeasurement && newMode == '3')
@@ -185,7 +184,7 @@ void readSerial()
       return;
     }
 
-    // es kann mehrmals nacheinander eine Variable gesetzt werden
+    // es könnte mehrmals nacheinander eine Variable gesetzt werden
     if (newMode != mode || (mode == '2' && newMode == '2'))
     {
       mode = newMode;
@@ -207,56 +206,97 @@ void readSerial()
   case '1':
     lastUpdateTime = millis();
     turnLasersOn();
-    // Serial.println("turned on!");
+    // LaserSerial.println("turned on!");
     break;
 
   case '2':
-    byte m = Serial.readBytesUntil('\n', varSerialData, SERIAL_DATA_LENGTH);
+    byte m = LaserSerial.readBytesUntil('\n', varSerialData, SERIAL_DATA_LENGTH);
 
-    if (m < 6)
+    if (m <= SERIAL_DATA_PREFIX_LENGTH)
       return;
 
     // add '\0'
     varSerialData[m] = '\0';
-    // Serial.println(varSerialData);
+    // LaserSerial.println(varSerialData);
 
+    // replace equal sign with null character
     varNameData[SERIAL_DATA_PREFIX_LENGTH - 1] = '\0';
     for (int i = 0; i < SERIAL_DATA_PREFIX_LENGTH - 1; i++)
     {
       varNameData[i] = varSerialData[i];
     }
 
-    // // remove the name in the array
+    // // remove prefix
     // memset(varSerialData, 0x30, SERIAL_DATA_PREFIX_LENGTH);
     for (int i = 0; i < SERIAL_DATA_LENGTH - SERIAL_DATA_PREFIX_LENGTH - 1; i++)
     {
       varValueData[i] = varSerialData[SERIAL_DATA_PREFIX_LENGTH + i];
     }
 
-    if (strcmp(varNameData, "PWM405") == 0)
+    if (strcmp(varNameData, "Cyc405") == 0)
     {
-      laser405Brightness = atoi(varValueData);
-      // Serial.println("Setting laser405Brightness");
-      // Serial.println(laser405Brightness, DEC);
+      pwmDutyVal405 = atoi(varValueData);
+      ledcWrite(LASER_PIN_405, pwmDutyVal405);
+      // LaserSerial.println("Setting pwmDutyVal405");
+      // LaserSerial.println(pwmDutyVal405, DEC);
     }
-    else if (strcmp(varNameData, "Del445") == 0)
+    else if (strcmp(varNameData, "Cyc445") == 0)
     {
-      laser445PulseDelay = atoi(varValueData);
-      // Serial.println("Setting laser445PulseDelay");
-      // Serial.println(laser445PulseDelay, DEC);
+      pwmDutyVal445 = atoi(varValueData);
+      if (!DISABLE_PWM_445)
+      {
+        ledcWrite(LASER_PIN_445, pwmDutyVal445);
+      }
+      // LaserSerial.println("Setting pwmDutyVal445");
+      // LaserSerial.println(pwmDutyVal445, DEC);
     }
-    else if (strcmp(varNameData, "Num445") == 0)
+    else if (strcmp(varNameData, "Frq405") == 0)
     {
-      laser445PulseNum = atoi(varValueData);
-      if (!MODE_SWICH_445)
-        osp_setup();
-      // Serial.println("Setting laser445PulseNum");
-      // Serial.println(laser445PulseNum, DEC);
+      pwmFreq405 = atoi(varValueData);
+
+      ledcChangeFrequency(LASER_PIN_405, pwmFreq405, pwmResBits405);
+      // LaserSerial.println("Setting pwmFreq405");
+      // LaserSerial.println(pwmFreq405, DEC);
+    }
+    else if (strcmp(varNameData, "Frq445") == 0)
+    {
+      pwmFreq445 = atoi(varValueData);
+      if (!DISABLE_PWM_445)
+      {
+        ledcChangeFrequency(LASER_PIN_445, pwmFreq445, pwmResBits445);
+      }
+      // LaserSerial.println("Setting pwmFreq445");
+      // LaserSerial.println(pwmFreq445, DEC);
+    }
+    else if (strcmp(varNameData, "Res405") == 0)
+    {
+      pwmResBits405 = atoi(varValueData);
+
+      // update
+      maxDutyVal405 = (uint16_t)(pow(2, pwmResBits405) - 1);
+
+      ledcChangeFrequency(LASER_PIN_405, pwmFreq405, pwmResBits405);
+
+      // LaserSerial.println("Setting pwmResBits405");
+      // LaserSerial.println(pwmResBits405, DEC);
+    }
+    else if (strcmp(varNameData, "Res445") == 0)
+    {
+      pwmResBits445 = atoi(varValueData);
+      // update
+      maxDutyVal445 = (uint16_t)(pow(2, pwmResBits445) - 1);
+
+      if (!DISABLE_PWM_445)
+      {
+        ledcChangeFrequency(LASER_PIN_445, pwmFreq445, pwmResBits445);
+      }
+      // LaserSerial.println("Setting pwmResBits445");
+      // LaserSerial.println(pwmResBits445, DEC);
     }
     else if (strcmp(varNameData, "SetLED") == 0)
     {
       String data = (String)atoi(varValueData); // z. B. 0000000222 zu 222
-      // Serial.println(data);
+      // LaserSerial.println(data);
       int r = data[0] - '0';
       int g = data[1] - '0';
       int b = data[2] - '0';
@@ -266,15 +306,15 @@ void readSerial()
     else if (strcmp(varNameData, "ConMea") == 0)
     {
       continuousMeasurement = atoi(varValueData);
-      // Serial.println("Setting continuousMeasurement");
-      // Serial.println(continuousMeasurement);
+      // LaserSerial.println("Setting continuousMeasurement");
+      // LaserSerial.println(continuousMeasurement);
     }
     else if (strcmp(varNameData, "ExpDel") == 0)
     {
       expectedDelay = atol(varValueData);
-      // Serial.println("Setting expectedDelay");
-      // Serial.println(varValueData);
-      // Serial.println(expectedDelay);
+      // LaserSerial.println("Setting expectedDelay");
+      // LaserSerial.println(varValueData);
+      // LaserSerial.println(expectedDelay);
     }
     // reset array
     memset(varSerialData, 0x00, SERIAL_DATA_LENGTH);
@@ -288,7 +328,10 @@ void readSerial()
 void setLED(byte r, byte g, byte b)
 {
 
-  // Serial.println("setting LED" + String(r));
+  if (!USE_RGB_LED)
+    return;
+
+  // LaserSerial.println("setting LED" + String(r));
   analogWrite(LED_R_PIN, r);
   analogWrite(LED_G_PIN, g);
   analogWrite(LED_B_PIN, b);
@@ -299,12 +342,17 @@ void enableLocks()
 
   digitalWrite(LASER_PIN_445_KILL_SWITCH, LOW);
   digitalWrite(LASER_PIN_SUPERCON, HIGH);
-  analogWrite(LASER_PIN_405, 0);
+
+  // nur den duty cycle auf 0 setzen würde vermutlich dazu führen, dass es beim Resetten des PWM-Timers einen voltage-spike von einem Clock-Cycle gibt.
+  ledcDetach(LASER_PIN_405);
+  digitalWrite(LASER_PIN_405, 0);
+
+  ledcDetach(LASER_PIN_445);
+  digitalWrite(LASER_PIN_445, 0);
 }
 
 void disableLocks()
 {
-
   digitalWrite(LASER_PIN_445_KILL_SWITCH, HIGH);
 }
 
@@ -312,41 +360,22 @@ void turnLasersOn()
 {
 
   disableLocks();
-  analogWrite(LASER_PIN_405, laser405Brightness);
-  // Serial.println("brightness: " + String(laser405Brightness));
+
+  ledcAttachChannel(LASER_PIN_405, pwmFreq405, pwmResBits405, PWM_CHANNEL_405);
+  ledcAttachChannel(LASER_PIN_445, pwmFreq445, pwmResBits445, PWM_CHANNEL_445);
+
+  ledcWrite(LASER_PIN_405, pwmDutyVal405);
+  // pull down resistor, thus inverted logic
   digitalWrite(LASER_PIN_SUPERCON, LOW);
 
-  while (true)
+  if (pwmDutyVal445 == 1234 || DISABLE_PWM_445)
   {
-
-    if (laser445PulseNum == 1234 || MODE_SWICH_445)
-    {
-      // TCCR2B = 0;
-      // LOW, HIGH, bringt alles nix
-      digitalWrite(LASER_PIN_445, HIGH);
-      // analogWrite(LASER_PIN_445, 255);
-    }
-    else
-    {
-
-      // You could wrap OSP_SET_AND_FIRE(o); in a non-blocking millis()-guarded loop to set the frequency to whatever you want.
-      OSP_FIRE();
-
-      while (OSP_INPROGRESS())
-        ; // This just shows how you would wait if nessisary - not nessisary in this application.
-
-      delay(laser445PulseDelay);
-    }
-    readSerial();
-    if (millis() - lastUpdateTime >= expectedDelay)
-    {
-      mode = '0';
-    }
-    if (mode != '1')
-    {
-      turnLasersOff();
-      break;
-    }
+    // LOW, HIGH, bringt alles nix
+    digitalWrite(LASER_PIN_445, HIGH);
+  }
+  else
+  {
+    ledcWrite(LASER_PIN_445, pwmDutyVal445);
   }
 }
 
@@ -354,4 +383,29 @@ void turnLasersOff()
 {
   enableLocks();
   lastUpdateTime = millis();
+}
+
+// median filter
+int readPoti()
+{
+  for (int i = 0; i < POTI_SAMPLE_COUNT; i++)
+  {
+    potiReadings[i] = analogRead(POT_PIN);
+    delay(2);
+  }
+
+  for (int i = 0; i < POTI_SAMPLE_COUNT - 1; i++)
+  {
+    for (int j = i + 1; j < POTI_SAMPLE_COUNT; j++)
+    {
+      if (potiReadings[j] < potiReadings[i])
+      {
+        int temp = potiReadings[i];
+        potiReadings[i] = potiReadings[j];
+        potiReadings[j] = temp;
+      }
+    }
+  }
+
+  return potiReadings[POTI_SAMPLE_COUNT / 2];
 }
